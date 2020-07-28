cf52a487ed16fc7b03330cde76520e29617d9c71
YairM <mazaly@post.bgu.ac.il>
2020-07-29 00:51:26 +0300

    initial commit delegator

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='OOP_utils',
+    version='1.0',
+    packages=['oop_utils'],
+    url='https://github.com/YairMZ/OOP_utils',
+    license='',
+    author='Yair Mazal',
+    author_email='mazaly@post.bgu.ac.il',
+    description='My implementation of various OOP paradigms and tools, not built-in into python',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
