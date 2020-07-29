0e7eb45b507032ce953396df9280eb8c3608af7a
YairM <mazaly@post.bgu.ac.il>
2020-07-30 00:40:59 +0300

    distro ready

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='This minimal yet powerful library allows delegation of an objects: methods, properties, and his own '
                 'member objects, to an object who owns it. Recursive delegation is also supported.'
                 'Use the simple API to handle delegations.',
-    download_url='https://github.com/YairMZ/ObjectDelegator/archive/v_10.tar.gz',    # I explain this later on
+    download_url='https://github.com/YairMZ/ObjectDelegator/archive/v_10.tar.gz',
     keywords=['OOP', 'delegation', 'composition'],
     classifiers=[
         "Programming Language :: Python :: 3",
