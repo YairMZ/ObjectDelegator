3240f805e1187c8827d2b3ebf8e63c4a34d60020
YairM <mazaly@post.bgu.ac.il>
2020-07-29 21:54:54 +0300

    Can now deleagate properties too. maybe even other objects

--- oop_utils/delegator.py
+++ oop_utils/delegator.py
@@ -12,89 +12,95 @@ def _custom_dir(c: Any, add: List[str]) -> list:
 # noinspection SpellCheckingInspection
 class Delegator(ABC):
     """
-    Abstract class to enable delegation of methods.
-    To use inherit it and use the API to delegate specific methods of chosen members.
+    Abstract class to enable delegation of members, properties, and methods.
+    To use, inherit it and use the API to delegate chosen members.
     """
-    _delegated_methods: dict = {}
+    _delegated_members: dict = {}
 
-    def get_delegated_methods(self) -> dict:
+    def get_delegated_members(self) -> dict:
         """
-        return all delegated methods
+        return all delegated members
 
         :rtype: dict
-        :return: returns a dictionary with keys as memebers which own delegated methods, values as list of string names of delegated methds per member.
+        :return: returns a dictionary with keys as memebers which own delegated members, values as list of string names of delegated members.
         """
-        return self._delegated_methods
+        return self._delegated_members
 
-    def set_delegated_methods(self, delegation_dict: dict) -> None:
-        """sets the delegated methods
+    def set_delegated_members(self, delegation_dict: dict) -> None:
+        """sets the delegated members
 
-        delegation dict should be a dictionary of members names as keys, and methods names as list of strings.
+        delegation dict should be a dictionary of members names as keys, and delegated members names as list of strings.
         Example:
 
         {'foo_obj': ['foo_method1], 'bar_obj': ['bar_method1', 'bar_method2']}
 
-        :raises AttributeError: The function raises an AttributeError if a certian member doesn't have the required method
-        :raises ValueError: The function raises a ValueError if the same method name appears twice.
+        :raises AttributeError: The function raises an AttributeError if a certian member doesn't have the required attribute
+        :raises ValueError: The function raises a ValueError if the same member name appears twice.
         """
-        method_names = []
-        [method_names.extend(x) for x in delegation_dict.values()]  # get all method names
-        if len(set(method_names)) < len(method_names):  # check for recurring names
-            raise ValueError("cannot delegate two methods with the same name.")
+        member_names = []
+        [member_names.extend(x) for x in delegation_dict.values()]  # get all member names
+        if len(set(member_names)) < len(member_names):  # check for recurring names
+            raise ValueError("cannot delegate two members with the same name.")
         for member in delegation_dict.keys():
-            for method_name in delegation_dict[member]:
-                if not callable(getattr(getattr(self, member), method_name)):
-                    raise AttributeError("'%s' object has no callable '%s'" % (member, method_name))
-        self._delegated_methods = delegation_dict
+            for member_name in delegation_dict[member]:
+                if not getattr(getattr(self, member), member_name, None):
+                    raise AttributeError("'%s' object has no attribute '%s'" % (member, member_name))
+        self._delegated_members = delegation_dict
 
-    def get_possible_methods_to_delegate(self, required_object: Union[None, str] = None) -> dict:
-        """Obtain a dict of methods which can be delegated for a single or all members.
+    def get_possible_members_to_delegate(self, required_object: Union[None, str] = None) -> dict:
+        """Obtain a dict of members which can be delegated.
 
-        :param required_object: if required_object is None possible methods are returned for all object members otherwise methods are returned for selected member.
+        :param required_object: if required_object is None possible members are returned for all members of self. otherwise  possible members are returned for required_object.
         :rtype: dict
-        :returns: a dict of keys as objects, and values as strings of method names whic hcan be delegated.
+        :returns: a dict of object names as keys, and values as strings of member names which hcan be delegated.
         """
         if required_object is None:
             possible_members = self.get_possible_objects_to_delegate_to()
             result = {}
             for member in possible_members:
                 result[member] = list(
-                    filter(lambda x: x[0] != '_' and callable(getattr(getattr(self, member), x)),
+                    filter(lambda x: x[0] != '_' and getattr(getattr(self, member), x, None),
                            dir(getattr(self, member))))
             return result
         else:
             return {required_object: list(
-                filter(lambda x: x[0] != '_' and callable(getattr(getattr(self, required_object), x)),
+                filter(lambda x: x[0] != '_' and getattr(getattr(self, required_object), x, None),
                        dir(getattr(self, required_object))))}
 
     def get_possible_objects_to_delegate_to(self):
         """This method is only relevant if called within a child class (when self is a subclassed object).
         Most probably won't raise any exceptions otherwise, but results will be confusing and misleading.
         """
-        return [obj for obj in dir(self) if not (obj.startswith('_') or callable(getattr(self, obj)))]
+        delegation_dict = self._delegated_members
+        self._delegated_members = {}
+        res = [obj for obj in dir(self) if not (obj.startswith('_') or callable(getattr(self, obj)))]
+        self.set_delegated_members(delegation_dict)
+        return res
 
     @property
-    def _list_all_delegated_methods(self):
-        """returns a flat list of all delegated methods. For internal use."""
-        if self._delegated_methods:
-            method_names = []
-            [method_names.extend(x) for x in self._delegated_methods.values()]  # get all method names
-            return method_names
+    def _list_all_delegated_members(self):
+        """returns a flat list of all delegated members. For internal use."""
+        if self._delegated_members:
+            member_names = []
+            [member_names.extend(x) for x in self._delegated_members.values()]  # get all member names
+            return member_names
         else:
             return []
 
     def __getattr__(self, _attribute):
-        for member in self._delegated_methods.keys():
-            if _attribute in self._delegated_methods[member]:
+        for member in self._delegated_members.keys():
+            if _attribute in self._delegated_members[member]:
                 return getattr(getattr(self, member), _attribute)
         raise AttributeError(_attribute)
 
     def __dir__(self):
-        return _custom_dir(self, self._list_all_delegated_methods)
+        return _custom_dir(self, self._list_all_delegated_members)
 
 
 if __name__ == '__main__':
     class Foo:
+        foo_property = 'hi'
+
         # noinspection PyMethodMayBeStatic
         def foo(self):
             """dummy method"""
@@ -106,12 +112,13 @@ if __name__ == '__main__':
             tmp = Foo()
             self.bar = tmp
             self.test = 1
-            self.set_delegated_methods({'bar': ['foo']})
+            self.set_delegated_members({'bar': ['foo', 'foo_property']})
 
 
     b = Bar()
     print(b.foo())
     test = dir(b)
     print(b.get_possible_objects_to_delegate_to())
-    print(b.get_possible_methods_to_delegate())
+    print(b.get_possible_members_to_delegate())
+    print(b.foo_property)
     pass
