9d6bfdcf67b8d4183cac0ea0d3914690c9fa5bb6
YairM <mazaly@post.bgu.ac.il>
2020-07-30 00:24:17 +0300

    ready for distro

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# OOP_utils
-My implementation of various OOP paradigms and tools, not built in into python
+# Object Delegator
+This minimal yet powerful library allows delegation of an objects: methods, properties, and his own
+member objects, to an object who owns it. Recursive delegation is also supported.
+Use the simple API to handle delegations.
--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ setup(
     name='ObjectDelegator',
     version='1.0',
     packages=['ObjectDelegator'],
-    url='https://github.com/YairMZ/OOP_utils',
-    license='',
+    url='https://github.com/YairMZ/ObjectDelegator',
+    license='GNU General Public License v3.0',
     author='Yair Mazal',
     author_email='mazaly@post.bgu.ac.il',
     description='This minimal yet powerful library allows delegation of an objects: methods, properties, and his own '
                 'member objects, to an object who owns it. Recursive delegation is also supported.'
                 'Use the simple API to handle delegations.',
+    download_url='https://github.com/YairMZ/ObjectDelegator/archive/v_10.tar.gz',    # I explain this later on
+    keywords=['OOP', 'delegation', 'composition'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Object Brokering",
     ],
     python_requires='>=3.8',
 )
