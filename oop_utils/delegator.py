ab85639c22220e82915aac11dd4d0a236a65b3a0
YairM <mazaly@post.bgu.ac.il>
2020-07-29 04:56:53 +0300

    Delegator class is now abstract. Added mtheods to obtain all possible subclassed members which on can delegate to.

--- oop_utils/delegator.py
+++ oop_utils/delegator.py
@@ -1,70 +1,51 @@
-import inspect
-
-
-# taken from https://www.fast.ai/2019/08/06/delegation/
-## not yet tested ##
-def delegates(to=None, keep=False):
-    """Decorator: replace `**kwargs` in signature with params from `to`"""
-    def _f(f):
-        if to is None:
-            to_f, from_f = f.__base__.__init__, f.__init__
-        else:
-            to_f, from_f = to, f
-        sig = inspect.signature(from_f)
-        sigd = dict(sig.parameters)
-        k = sigd.pop('kwargs')
-        s2 = {k: v for k, v in inspect.signature(to_f).parameters.items()
-              if v.default != inspect.Parameter.empty and k not in sigd}
-        sigd.update(s2)
-        if keep:
-            sigd['kwargs'] = k
-        from_f.__signature__ = sig.replace(parameters=sigd.values())
-        return f
-    return _f
+from abc import ABC
 
 
 # taken from https://www.fast.ai/2019/08/06/delegation/
 def custom_dir(c, add): return dir(type(c)) + list(c.__dict__.keys()) + add
 
+
 # based on https://www.fast.ai/2019/08/06/delegation/
 # noinspection SpellCheckingInspection
-class Delegator:
+class Delegator(ABC):
     """
     Base class for attr accesses in `self._xtra` passed down to `self.default`
     """
-    xtra = []
+    _delegated_methods = []
 
-    @property
-    def xtra(self):
-        return self._xtra
-
-    @xtra.setter
-    def xtra(self, val):
-        if self.default is not None:
-            self._xtra = val
-        else:
-            self._xtra = []
+    def get_delegated_methods(self):
+        return self._delegated_methods
 
     def set_delegated_methods(self, methods_list=None):
-        if methods_list is None:
-            self.xtra = self.get_possible_methods_to_delegate()
+        if self.default is not None:
+            if methods_list is None:
+                self._delegated_methods = self.get_possible_methods_to_delegate()
+            else:
+                self._delegated_methods = methods_list
         else:
-            self.xtra = methods_list
+            self._delegated_methods = []
 
     def get_possible_methods_to_delegate(self):
         if self.default is None:
             return []
         else:
-            return [o for o in dir(self.default) if not o.startswith('_')]
+            return list(filter(lambda x: x[0] != '_' and callable(getattr(self.default, x)), dir(self.default)))
+            # [o for o in dir(self.default) if not o.startswith('_')]
 
+    def get_possible_objects_to_delegate_to(self):
+        """This method is only relevant if called within a child class (when self is a subclassed object).
+        Most probably won't raise any exceptions otherwise, but results will be confusing and misleading.
+        """
+        return [obj for obj in dir(self) if not (obj.startswith('_') or callable(getattr(self, obj)))]
 
     def __getattr__(self, k):
-        if k in self.xtra:
+        if k in self._delegated_methods:
             return getattr(self.default, k)
         raise AttributeError(k)
 
     def __dir__(self):
-        return custom_dir(self, self.xtra)
+        return custom_dir(self, self._delegated_methods)
+
 
 
 if __name__ == '__main__':
@@ -76,10 +57,16 @@ if __name__ == '__main__':
         def __init__(self):
             tmp = Foo()
             self.bar = tmp
+            self.test = 1
             self.default = tmp
             self.set_delegated_methods()
 
+
     b = Bar()
     print(b.foo())
+    test1 = [o for o in dir(b) if not o.startswith('_')]
+    test2 = [o for o in test1 if not callable(getattr(b,o))]
+    test = [o for o in dir(b) if not (o.startswith('_') or callable(getattr(b,o))) ]
+    print(b.get_possible_objects_to_delegate_to())
     print(b.get_possible_methods_to_delegate())
     pass
